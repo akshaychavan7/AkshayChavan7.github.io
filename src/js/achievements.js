93a7b53f51362df190f7c71dfd41d48063d28985
Akshay Chavan <chavan.ak@northeastern.edu>
2023-12-30 00:42:14 -0500

    add achievements section and its data

---
+++ src/js/achievements.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  mapAchievementsData();
+});
+
+function mapAchievementsData() {
+  let achievements = data.achievements;
+  const achievementsRowEle = document.getElementById("achievements-row");
+  for (let achievement of achievements) {
+    let achievementEle = document.createElement("div");
+    achievementEle.classList.add("col-sm-4");
+    let labelsHTML = "";
+    for (let label of achievement.labels) {
+      labelsHTML += `
+                    <a
+                      href="${label.link}"
+                      class="achievement-tag"
+                      target="_blank"
+                      ><span class="label credentials-btn">
+                        <img
+                          class="check"
+                          src="${label.icon}"
+                          width="14"
+                          height="14"
+                        />
+                        ${label.tag}
+                      </span>
+                      </a>
+                    `;
+    }
+    const cardInnerHTML = `
+    <div class="achievement">
+              <div>
+                <div id="title" class="achievement-title">
+                  ${achievement.title}
+                </div>
+                <div id="event-date" class="achievement-date">${achievement.date}</div>
+              </div>
+              <div id="description" class="achievement-description">
+               ${achievement.description}
+              </div>
+              <div id="tags" class="achievement-tags">
+              ${labelsHTML}
+              </div>
+            </div>
+    `;
+    achievementEle.innerHTML = cardInnerHTML;
+    achievementsRowEle.appendChild(achievementEle);
+  }
+}
